f5df5a299ce9d434e464a8ef4bb9fd7dc40fe822
ito0804takuya <ito0804takuya@gmail.com>
2021-03-14 18:05:54 +0900

    タスク全件取得query実装

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -7,11 +7,18 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    # sample
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
     def test_field
       "Hello World!"
+    end
+
+    # タスク
+    field :tasks, [Types::TaskType], null: false, description: 'タスクを全件取得する'
+    def tasks
+      Task.all
     end
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_14_081508) do
+
+  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
