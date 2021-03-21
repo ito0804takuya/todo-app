b3ebd7ec303904bf9d17bd11124e1e2c424912a8
ito0804takuya <ito0804takuya@gmail.com>
2021-03-22 08:11:28 +0900

    フロント ビュー作成, apollo導入

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,10 +1,23 @@
+import colors from 'vuetify/es5/util/colors'
 
 export default {
-  mode: 'universal',
+  mode: 'spa',
   /*
   ** Headers of the page
   */
+  // head: {
+  //   title: process.env.npm_package_name || '',
+  //   meta: [
+  //     { charset: 'utf-8' },
+  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  //     { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+  //   ],
+  //   link: [
+  //     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  //   ]
+  // },
   head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
@@ -12,7 +25,12 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
     ]
   },
   /*
@@ -38,7 +56,30 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+
+    '@nuxtjs/vuetify',
+    // '@nuxtjs/pwa',
+    // '@nuxtjs/eslint-module',
+
+    '@nuxtjs/apollo', // apolloクライアント
   ],
+  
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    theme: {
+      primary: colors.blue.darken2,
+      accent: colors.grey.darken3,
+      secondary: colors.amber.darken3,
+      info: colors.teal.lighten1,
+      warning: colors.amber.base,
+      error: colors.deepOrange.accent4,
+      success: colors.green.accent3
+    }
+  },
+  
   /*
   ** Build configuration
   */
@@ -48,5 +89,16 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+
+  apollo: {
+    clientConfigs: {
+      default: '~/apollo/client-configs/default.js'
+      // httpEndpoint: 'http://localhost:3000/graphql'
+    }
+  }
+  ,
+  server: {
+    port: 8080
   }
 }
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,72 +1,122 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        todo-app
-      </h1>
-      <h2 class="subtitle">
-        My ultimate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="6" offset="3">
+        <v-card>
+          <v-card-title>Register Task</v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col cols="6">
+                    <v-text-field v-model="newTask.title" label="Title" required />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field v-model="newTask.description" label="Description" required />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-btn @click="createTask(newTask)">
+                      Register
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row v-for="task in tasks" :key="task.id">
+      <v-col cols="6" offset="3">
+        <v-card>
+          <v-card-title>
+            <v-checkbox v-model="task.completed" :label="task.title" @click="updateTask(task)" />
+            <v-btn text icon @click="deleteTask(task)">
+              <v-icon>clear</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text>{{ task.description }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import tasks from '~/apollo/queries/tasks'
+import createTask from '~/apollo/mutations/createTask'
+import deleteTask from '~/apollo/mutations/deleteTask'
+import updateTask from '~/apollo/mutations/updateTask'
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      tasks: {},
+      newTask: {
+        title: '',
+        description: ''
+      }
+    }
+  },
+  methods: {
+    async createTask(task) {
+      try {
+        await this.$apollo.mutate({
+          mutation: createTask,
+          variables: {
+            id: task.id,
+            title: task.title,
+            description: task.description
+          },
+          refetchQueries: [{
+            query: tasks
+          }]
+        })
+        this.newTask = { title: '', description: '' }
+      } catch (e) {
+        window.console.log(e)
+      }
+    },
+    async deleteTask(task) {
+      try {
+        await this.$apollo.mutate({
+          mutation: deleteTask,
+          variables: {
+            id: task.id
+          },
+          refetchQueries: [{
+            query: tasks
+          }]
+        })
+      } catch (e) {
+        window.console.log(e)
+      }
+    },
+    async updateTask(task) {
+      try {
+        await this.$apollo.mutate({
+          mutation: updateTask,
+          variables: {
+            id: task.id,
+            completed: !task.completed
+          },
+          refetchQueries: [{
+            query: tasks
+          }]
+        })
+      } catch (e) {
+        window.console.log(e)
+      }
+    }
+  },
+  apollo: {
+    tasks: {
+      query: tasks
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
